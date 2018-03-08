909e2463102230eee6f4e77840a34efee4537ab0
Beata Czekaj <czekajbeata@onet.pl>
2018-03-08 19:08:41 +0100

    finished section 6

---
+++ Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public byte Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public byte GenreId { get; set; }
+
+        [Required]
+         public DateTime ReleaseDate { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        [Required]
+        [Range(1, 20)]
+        public byte NumberInStock { get; set; }
+    }
+}
